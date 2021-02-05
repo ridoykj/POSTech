5cd9d7f92eb282bd34c939568d88d16c560d89cc
Ridoy Kumar Joy <ridoykj@gmail.com>
2021-02-06 00:12:20 +0600

    initial

---
+++ src/main/java/bd/postech/wo/PosTechApplication.java
@@ -0,0 +1,13 @@
+package bd.postech.wo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bd/postech/wo/PosTechApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.postech.wo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
