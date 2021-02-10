16a49d8a1cf2b897a60b1388981d15766e8c1b15
Ridoy Kumar Joy <ridoykj@gmail.com>
2021-02-10 22:29:08 +0600

    initial model

--- src/main/java/bd/postech/wo/model/Init.java
+++ src/main/java/bd/postech/wo/model/Init.java
@@ -1,23 +1,36 @@
 package bd.postech.wo.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
-@Entity(name = "T_user_info")
+@Entity
+@Table(name = "T_user_info")
 @Data
 public class Init {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Column(name = "var")
+	private Integer var;
+	
+	@Column(name = "dtt")
+	private Date dtt;	
+	
 	@Column(name = "Name")
 	private String name;
+	
 	@Column(name = "Phone")
 	private String phone;
+	
 	@Column(name = "Email")
 	private String email;
 
